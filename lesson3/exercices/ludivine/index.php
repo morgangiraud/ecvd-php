33c7a81bf0f13a573196600e0648cbc623ffaf66
Ludivine Maechler <maechler.ludivine@gmail.com>
2015-11-04 00:16:57 +0100

    Work lesson3 PHP

--- lesson3/exercices/ludivine/index.php
+++ lesson3/exercices/ludivine/index.php
@@ -33,22 +33,40 @@
 </form>
 
 
-    <?php
-        $file = "users.txt";
-        if(isset($_POST['nom']) && isset($_POST['mdp']) && isset($_POST['email'])) {
+<?php
+
+    echo trim($_POST["nom"]);
+    echo trim($_POST["mdp"]);
+    echo trim($_POST["email"]);
+
+        $file = file("users.txt");
+            if(isset($_POST['nom']) && empty($_POST['nom']) == false && isset($_POST['mdp']) && empty($_POST['mdp']) == false && isset($_POST['email']) && empty($_POST['email']) == false) {
             $data = $_POST['nom'] . '.+.' . $_POST['mdp'] . '.+.' . $_POST['email'] . "\n";
-            $ret = file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-            if ($ret === false) {
-                die ('There was an error writing this file');
-            }
-            else {
-                echo "$ret";
+            var_dump($file);
+            foreach($file as $line) {
+
+                if($line == $data) {
+                    echo $data . " is in the users.txt";
+                    
+                    $_SESSION['login_user']=$_POST['nom'];
+                    $_SESSION['pwd_user']=$_POST['mdp'];
+
+                    header('Location: connect.php');
+                     exit;
+                }
             }
         }
         else {
            die('no post data to process');
         }
-    ?>
+?>
+
+<?php
+
+echo md5($_POST["mdp"]);
+
+
+?>
 
 </body>
 </html>
--- lesson3/exercices/ludivine/login.php
+++ lesson3/exercices/ludivine/login.php
@@ -1,6 +1,8 @@
-<?php session_start();
+<?php 
+session_start();
+session_regenerate_id();
     
-    if ($_SESSION['login_user'] && $_SESSION['pwd_user']) {
+    if (isset($_SESSION['login_user']) && isset($_SESSION['pwd_user'])) {
         header('Location: connect.php');
         exit;
         
@@ -37,8 +39,12 @@
 
 <?php
 
+    echo trim($_POST["nom"]);
+    echo trim($_POST["mdp"]);
+    echo trim($_POST["email"]);
+
         $file = file("users.txt");
-            if(isset($_POST['nom']) && isset($_POST['mdp']) && isset($_POST['email'])) {
+            if(isset($_POST['nom']) && empty($_POST['nom']) == false && isset($_POST['mdp']) && empty($_POST['mdp']) == false && isset($_POST['email']) && empty($_POST['email']) == false) {
             $data = $_POST['nom'] . '.+.' . $_POST['mdp'] . '.+.' . $_POST['email'] . "\n";
             var_dump($file);
             foreach($file as $line) {
@@ -48,12 +54,22 @@
                     
                     $_SESSION['login_user']=$_POST['nom'];
                     $_SESSION['pwd_user']=$_POST['mdp'];
+
+                    header('Location: connect.php');
+                     exit;
                 }
             }
         }
         else {
            die('no post data to process');
         }
+?>
+
+<?php
+
+echo md5($_POST["mdp"]);
+
+
 ?>
 
 </body>
