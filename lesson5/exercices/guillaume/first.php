bf3f74bacaeb9ddcce1b71404b8acfaa506c1992
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2015-11-12 10:15:42 +0100

    add password security better and login better too with sql protection

---
+++ lesson5/exercices/guillaume/first.php
@@ -0,0 +1,93 @@
+<?php session_start(); ?>
+
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+		<title>Test PHP</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<?php
+
+			// On récupère la connexion à la BDD
+
+			function get_user($username) {
+				require_once('connect.php');
+				// On récupère les utilisateurs enregistrés dans la base de données
+				$response = $bdd->prepare("SELECT * FROM `users` WHERE `username` = ?");
+				$response->execute(array($username));
+				return $response->fetchAll();
+			}
+
+			// Si l'utilisateur est déjà enregistré en session, on lui propose de se déconnecter
+			if(isset($_SESSION['username'])) {
+				echo 'Vous êtes bien connecté !';
+				echo '<a href="logout.php">Disconnect</a><br />';
+				echo '<a href="profile.php">Voir mon profil</a>';
+
+			// Sinon, on vérifie que les username et password entrés correspondent à un utilisateur
+			} else if(isset($_POST['name']) && !isset($_SESSION['username'])) {
+
+				$result = '';
+
+				if(!empty($_POST['name']) && !empty($_POST['password'])) {
+
+					$data = get_user($_POST['name']);
+
+					// Pour chaque utilisateur, on check
+					if(isset($data[0])) {
+
+						if(password_verify($_POST['password'], $data[0]['password'])) {
+
+							echo 'Vous êtes bien connecté !';
+							$_SESSION['username'] = $_POST['name'];
+							echo '<a href="logout.php">Disconnect</a><br />';
+							echo '<a href="profile.php">Voir mon profil</a>';
+						} else {
+							$result = 'Votre mot de passe ne correspond pas';
+						}
+
+
+					} else {
+						$result = 'Votre username ne correspond pas.';
+					}
+
+				} else {
+					$result = 'Remplissez tous les champs.';
+				}
+
+				// Sinon, on lui affiche l'erreur
+				if($result !== '') {
+					echo $result;
+				}
+
+			} else { ?>
+
+				<form action="first.php" method="post">
+
+					<div class="style_input">
+						<label for="name"></label>
+						<input type="text" id="name" name="name" />
+					</div>
+
+					<div class="style_input">
+						<label for="password"></label>
+						<input type="password" id="password" name="password" />
+					</div>
+					
+					<button>Envoyer</button>
+				</form>
+
+				<a href="register.php">Register</a>
+
+			<?php }
+
+		?>
+		
+	</body>
+
+</html>
---
+++ lesson5/exercices/guillaume/register.php
@@ -0,0 +1,72 @@
+<?php session_start();
+
+if(isset($_SESSION['username'])) {
+	header("Location: first.php");
+	exit();
+}
+?>
+
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+		<title>Test PHP</title>
+		<meta charset="utf-8" />
+	</head>
+
+	<body>
+
+		<?php
+
+			
+
+			function insert_user($username, $password) {
+				require_once('connect.php');
+
+				$password = password_hash($password, PASSWORD_BCRYPT);
+
+				try {
+					$insert = $bdd->prepare("INSERT INTO `users` (`username`, `password`, `email`, `description`) VALUES (?, ?, '', 'Ceci est une description tirée de la BDD du user');");
+					$insert->execute(array($username, $password));
+				} catch (Exception $e) {
+					var_dump($e);
+					die;
+				}	
+			}
+
+			if(isset($_POST['name'])) {
+
+				insert_user($_POST['name'], $_POST['password']);
+
+				$_SESSION['username'] = $_POST['name'];
+
+				header("Location: first.php");
+				exit();
+
+			} else { ?>
+
+				<form action="register.php" method="post">
+
+					<div class="style_input">
+						<label for="name"></label>
+						<input type="text" id="name" name="name" />
+					</div>
+
+					<div class="style_input">
+						<label for="password"></label>
+						<input type="password" id="password" name="password" />
+					</div>
+					
+					<button>Envoyer</button>
+				</form>
+
+				<a href="first.php">Login</a>
+
+			<?php }
+
+		?>
+		
+	</body>
+
+</html>
