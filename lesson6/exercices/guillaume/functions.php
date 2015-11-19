f4ded63d894772d1cfdc92af0a372036e1662bbe
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2015-11-19 10:31:11 +0100

    add exercice 6 with namespace and refactor better code

--- lesson6/exercices/guillaume/functions.php
+++ lesson6/exercices/guillaume/functions.php
@@ -5,15 +5,15 @@ namespace Ecvdphp;
 class User {
 
 	// Fonction d'insertion d'un user en BDD
-	function insert_user($username = '', $password = '') {
+	public static function insertUser($username = '', $password = '') {
 		require_once('connect.php');
 
 		$password = password_hash($password, PASSWORD_BCRYPT);
 
 		try {
 			$insert = $bdd->prepare("INSERT INTO `users` (`username`, `password`, `email`, `description`) VALUES (:username, :password, '', 'Ceci est une description tirée de la BDD du user');");
-			$insert->bindParam(':username', $username, PDO::PARAM_STR);
-			$insert->bindParam(':password', $password, PDO::PARAM_STR);
+			$insert->bindParam(':username', $username, \PDO::PARAM_STR);
+			$insert->bindParam(':password', $password, \PDO::PARAM_STR);
 			$insert->execute();
 		} catch (Exception $e) {
 			die("Some error occured while the register process : ".$e);
@@ -21,11 +21,11 @@ class User {
 	}
 
 	// On récupère la connexion à la BDD
-	function getUser($username = '') {
+	public static function getUser($username = '') {
 		require_once('connect.php');
 		// On récupère les utilisateurs enregistrés dans la base de données
 		$response = $bdd->prepare("SELECT * FROM `users` WHERE `username` = :username");
-		$response->bindParam(':username', $username, PDO::PARAM_STR);
+		$response->bindParam(':username', $username, \PDO::PARAM_STR);
 		$response->execute();
 		return $response->fetchAll();
 	}
